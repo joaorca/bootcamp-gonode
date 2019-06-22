4de790184daa62e683b4ed5faa1c9bfebbc46f98
João R C Almeida <joaorca@gmail.com>
2019-06-21 23:36:48 -0300

    Aula 08

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ nunjucks.configure("views", {
 
 app.set("view engine", "njk");
 
+const users = ["Diego Fernandes", "Robson Marques", "Cleiton Souza"];
+
 app.get("/", (req, res) => {
-  return res.render("list", { name: "João" });
+  return res.render("list", { users });
+});
+
+app.get("/new", (req, res) => {
+  return res.render("new");
 });
 
 app.listen(8080);
