5ab0b407e23c47bdfb01a19a1fa7d27cdfccb427
João R C Almeida <joaorca@gmail.com>
2019-06-22 00:08:29 -0300

    Aula 12

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
 
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
-});
+})
 
-app.use(express.urlencoded({ extended: false }));
-app.set("view engine", "njk");
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
 
-const users = ["Diego Fernandes", "Robson Marques", "Cleiton Souza"];
+const users = ['Diego Fernandes', 'Robson Marques', 'Cleiton Souza']
 
-app.get("/", (req, res) => {
-  return res.render("list", { users });
-});
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
 
-app.get("/new", (req, res) => {
-  return res.render("new");
-});
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
 
-app.post("/create", (req, res) => {
-  users.push(req.body.user);
-  return res.redirect("/");
-});
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
 
-app.listen(8080);
+app.listen(8080)
 
-process.on("SIGINT", () => process.exit());
+process.on('SIGINT', () => process.exit())
